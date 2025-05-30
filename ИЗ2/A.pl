443118cd763d0b6bcaa15640e8a4b0855e4729ce
Efl <133080730+Extrafly1@users.noreply.github.com>
2025-05-30 03:02:56 +0300

    Add neighbor processing with heuristic estimation

--- ИЗ2/A.pl
+++ ИЗ2/A.pl
@@ -15,3 +15,24 @@ insert_sorted([H | T], E, [E, H | T]) :-
     F1 =< F2, !.
 insert_sorted([H | T], E, [H | R]) :-
     insert_sorted(T, E, R).
+
+% Предикат обработки соседей текущей вершины
+process_neighbors([], _, _, _, Open, Open, _).
+process_neighbors([Neighbor-CostEdge | Ns], Node, G0, Goal, Open0, Open, Closed) :-
+    G1 is G0 + CostEdge,
+    (member(closed_node(Neighbor, _, _), Closed)
+    -> process_neighbors(Ns, Node, G0, Goal, Open0, Open, Closed)
+    ;  h(Neighbor, Goal, H),
+       F is G1 + H,
+       (select(fgv(_, OldG, Neighbor, _), Open0, Open1)
+       -> (G1 < OldG 
+           -> insert_sorted(Open1, fgv(F, G1, Neighbor, Node), Open2)
+           ;  Open2 = Open0
+          )
+       ;  insert_sorted(Open0, fgv(F, G1, Neighbor, Node), Open2)
+       ),
+       process_neighbors(Ns, Node, G0, Goal, Open2, Open, Closed)
+    ).
+
+% Временная заглушка эвристической функции
+h(_, _, 0).
